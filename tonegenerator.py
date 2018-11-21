59afeeb781a21018c5c9b2708c93cb4bb4f88805
Jakob <jc220866@falmouth.ac.uk>
2018-11-21 13:49:32 +0000

    fixed the issue with the sinewave having a 0 every other frame, ashley changed the 'i' in the struct pack to 'h' (for 'long')

--- tonegenerator.py
+++ tonegenerator.py
@@ -20,7 +20,7 @@ seconds = 1
 N_CHANNELS = 1
 SAMPLE_WIDTH = 2
 FRAMERATE = 44100  # SAMPLE_RATE
-N_FRAMES = (FRAMERATE * seconds)  # SAMPLE_LENGTH
+N_FRAMES = int((FRAMERATE * seconds))  # SAMPLE_LENGTH
 COMP_TYPE = "NONE"
 COMP_NAME = "not compressed"
 AMPLITUDE = 32767  # BIT_DEPTH or MAX_VALUE - any higher than 32778 creates an awful noise
@@ -49,7 +49,7 @@ def generate_tones(amount_of_tones):
         print((FREQUENCY + frequency_modifier))
         for j in range(0, N_FRAMES):
             part_of_a_sinewave = math.sin(2.0 * PI * (FREQUENCY + frequency_modifier) * (j / float(FRAMERATE))) * (VOLUME * (AMPLITUDE * amplitude_modifier))
-            print(part_of_a_sinewave)
+            # print(part_of_a_sinewave)
             tone.append(part_of_a_sinewave)
         frequency_modifier = (frequency_modifier + 440)
         tones.append(tone)
@@ -63,7 +63,7 @@ def create_tone():
     combined_tone = generate_tones(amount_of_tones)
     for i in range(0, len(combined_tone)):
 
-        packaged_value = struct.pack("i", int(combined_tone[i]))
+        packaged_value = struct.pack("h", int(combined_tone[i]))
 
         for j in range(0, N_CHANNELS):
             values.append(packaged_value)
@@ -77,10 +77,20 @@ def combine_tones(list_of_tones):
 
     combined_tone = []
 
+    for item in list_of_tones:
+        print('hi')
+
+    print(len(list_of_tones))
+    print((len(list_of_tones[0]) / 4), ' - this should not be ', str(N_FRAMES))
+    # print(len((list_of_tones[0] + list_of_tones[1] + list_of_tones[2] + list_of_tones[3])))
+
     for i in range(0, len(list_of_tones[0])):
-        combined_tone.append(0)
+        tone_sample = 0.0
         for tone in list_of_tones:
-            combined_tone[i] = combined_tone[i] + tone[i]
+            print(float(tone[i]))
+            tone_sample = tone_sample + float(tone[i])
+            # print(tone_sample)
+        combined_tone.append(tone_sample)
 
     print(combined_tone)
 
